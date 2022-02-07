05df0a5356736c39b844cf1de119d9fa765b53b3
sltKaguya <bottboris@gmail.com>
2022-02-07 15:46:31 +0300

    lab 01: misc (initialize classes)

---
+++ 01_oop_classes/01_main.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Dot {
+    int dimension;
+    std::vector<float> positions(float);
+};
+
+class BrokenLine {
+};
+
+class ClosedBroken {
+};
+
+class Polygon {
+};
+
+class Triangle {
+};
+
+class Trapeze {
+};
+
+class RegularPolygon {
+};
