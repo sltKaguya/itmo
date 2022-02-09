ed7ace76f342a10ce0cf4c59d80113d8909e0339
sltKaguya <bottboris@gmail.com>
2022-02-09 13:15:13 +0300

    misc fixed std explanation

--- simple_cpp/hello_world.cpp
+++ simple_cpp/hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream> //input-output library
 
-int main() { //std -- standart library including common functions e.g. cout
+int main() { //std -- standart namespace including common functions e.g. cout
     std::cout << "Hello World!\n"; //cout -- output command to the command line
     return 0;
 }
