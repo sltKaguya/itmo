59ae68b027f9b4f93e8665eb1725141ddb01d18e
sltKaguya <bottboris@gmail.com>
2022-02-23 00:40:00 +0300

    merge ctors, add getters, operator= and printvalue

--- prog/01_oop_classes/src/point.cpp
+++ prog/01_oop_classes/src/point.cpp
@@ -1,9 +1,30 @@
 #include "../include/point.h"
 
-Point::Point() {}
-
-Point::Point(int a, int b): x(a), y(b) {}
+Point::Point(int x_val, int y_val): x(x_val), y(y_val) {}
 
 Point::Point(const Point &other): x(other.x), y(other.y) {}
 
 Point::~Point() {}
+
+int Point::x_val() const {return x;}
+
+int Point::y_val() const {return y;}
+
+Point& Point::operator=(const Point &other) {
+    if (&other == this) {
+        return *this;
+    }
+
+    x = other.x;
+    y = other.y;
+    return *this;
+}
+
+void PrintPoint(Point &point) {
+    std::cout << "("
+            << point.x_val()
+            << "; "
+            << point.y_val()
+            << ")"
+            << std::endl;
+}
