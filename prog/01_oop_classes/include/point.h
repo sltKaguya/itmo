6705c8ca3d04055e207e0afd171bf04e486e4d86
sltKaguya <bottboris@gmail.com>
2022-02-23 00:38:52 +0300

    add normal documentation, merge default and nondefault ctors, getters, printpoint

--- prog/01_oop_classes/include/point.h
+++ prog/01_oop_classes/include/point.h
@@ -1,22 +1,56 @@
 #pragma once
 
-/**
- * I. Define Point class with 2 position on X and Y axes:
- ** 1) void constructor;
- ** 2) consructor with given arguments;
- ** 3) copy constructor;
- ** 4) destructor 
-**/
+#include <iostream>
+
 class Point {
     public:
-        Point();
+    /**
+     * Construct a new Point object with given values or zeros for default
+     * 
+     * x_val - position on X-axis
+     * y_val - position on Y-axis
+     */
+    Point(int x_val=0, int y_val=0);
 
-        Point(int a, int b);
+    /**
+     * Construct a copy of a given Point object
+     * 
+     * other - Point object to copy
+     */
+    Point(const Point &other);
 
-        Point(const Point &other);
+    /**
+     * Destroy object
+     */
+    ~Point();
 
-        ~Point();
+    /**
+     * Get the value on X-axis
+     */
+    int x_val() const;
+
+    /**
+     * Get the value on Y-axis
+     */
+    int y_val() const;
+
+    /**
+     * Assign one Point object values to another existing
+     * 
+     * other - Point object to assign
+     */
+    Point& operator =(const Point &other);
 
     private:
-        int x, y;
+    /**
+     * Positions on X and Y axes
+     */
+    int x, y;
 };
+
+/**
+ * Print given Point objects values on X and Y axes
+ * 
+ * point - Point object to print
+ */
+void PrintPoint(Point &point);
