56bfd00e274c3f67e8df4c4c0dcfbe38f99c8c8d
sltKaguya <bottboris@gmail.com>
2022-02-24 21:08:54 +0300

    Prog:Lab1: include headers + testing functions

--- prog/01_oop_classes/src/main.cpp
+++ prog/01_oop_classes/src/main.cpp
@@ -1,8 +1,12 @@
 #include "../include/point.h"
+#include "../include/polychain.h"
+#include "../include/closedchain.h"
+#include "../include/polygon.h"
 
 int main() {
     Point a {3, 4};
     Point b {2, 3};
+    Point d {5, 6};
     
     Point c = a;
     PrintPoint(c);
@@ -10,5 +14,67 @@ int main() {
     c = b;
     PrintPoint(c);
 
+    PolygonalChain myChain;
+
+    myChain.PushBack(&a);
+    myChain.PushBack(&b);
+    myChain.PushBack(&b);
+    myChain.PushBack(&a);
+
+    PolygonalChain secondChain;
+
+    secondChain.PushBack(&b);
+    secondChain.PushBack(&a);
+    secondChain.PushBack(&d);
+
+    PolygonalChain thirdChain = secondChain;
+    thirdChain = myChain;
+
+    PrintChain(secondChain);
+
+    std::cout << "Lenght of second Chain is " 
+            << secondChain.Perimeter()
+            << std::endl;
+
+    ClosedChain closed;
+    PrintChain(closed);
+    std::cout << "Lenght of closed Chain is "
+            << closed.Perimeter()
+            << std::endl;
+    closed.PushBack(&a);
+    PrintChain(closed);
+    std::cout << "Lenght of closed Chain is "
+            << closed.Perimeter()
+            << std::endl;
+    closed.PushBack(&b);
+    PrintChain(closed);
+    std::cout << "Lenght of closed Chain is "
+            << closed.Perimeter()
+            << std::endl;
+    closed.PushBack(&d);
+    PrintChain(closed);
+    std::cout << "Lenght of closed Chain is "
+            << closed.Perimeter()
+            << std::endl;
+    closed.PushBack(&d);
+    PrintChain(closed);
+    std::cout << "Lenght of closed Chain is "
+            << closed.Perimeter()
+            << std::endl;
+
+    Point one {0, 0};
+    Point two {2, 0};
+    Point three {2, 2};
+    Point four {0, 2};
+
+    Polygon square;
+    square.PushBack(&one);
+    square.PushBack(&two);
+    square.PushBack(&three);
+    square.PushBack(&four);
+    PrintChain(square);
+    std::cout << "Area of this square is "
+            << square.Area()
+            << std::endl;
     return 0;
 }
