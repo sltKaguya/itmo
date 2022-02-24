7c3c8689c06d2cebd19cbba3314e0ae5e58296e5
sltKaguya <bottboris@gmail.com>
2022-02-25 01:53:27 +0300

    Prog:Lab1: create Trapeze but PrintChain doesn't work for some reason

--- prog/01_oop_classes/include/trapeze.h
+++ prog/01_oop_classes/include/trapeze.h
@@ -2,7 +2,30 @@
 
 #include "polygon.h"
 
-class Trapeze: public ClosedChain {
+class Trapeze: public Polygon {
     public:
+    /**
+     * Create a new empty Trapeze object
+     */
+    Trapeze();
+
+    /**
+     * Add a Point in the Trapeze if possible
+     * No more than 4 Points
+     * base and top should be parallel
+     * left and right shouldn't
+     */
+    void PushBack(Point *point);
+
     private:
+    /**
+     * Coefficient k in y = kx + b, base, right side, top, left side
+     */
+    std::vector<double> k;
+
+    /**
+     * Offset of x and y from Point 1 to Point 2 in first case
+     * The offset from Point 3 to Point 4 in second case must be of opposite sign
+     */
+    std::vector<double> x_offset, y_offset;
 };
