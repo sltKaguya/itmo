82243d3fbe1c2883dbde6f387224f62d1a09e2ac
sltKaguya <bottboris@gmail.com>
2022-02-25 03:25:08 +0300

    Prog:Lab1: start making Polynomial class

---
+++ prog/01_oop_classes/include/polynomial.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Node {
+    public:
+
+    private:
+    /**
+     * Coefficient value
+     */
+    double value;
+
+    /**
+     * Power of the coefficient's argument
+     */
+    int pow;
+}
