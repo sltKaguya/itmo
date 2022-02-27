e0b3e9143dd2d465414bf4541789425de204be1e
sltKaguya <bottboris@gmail.com>
2022-02-27 18:22:21 +0300

    Algo:Lab8 task b

--- algo_ds/08_graphs_intro_bts/08_b.go
+++ algo_ds/08_graphs_intro_bts/08_b.go
@@ -1,1 +1,55 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fin, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(fin)
+	scanner.Split(bufio.ScanLines)
+
+	scanner.Scan()
+	split := strings.Fields((scanner.Text()))
+	size, _ := strconv.Atoi(split[0])
+
+	matrix := make([][]int, size)
+	for i := range matrix {
+		matrix[i] = make([]int, size)
+	}
+
+	for i := 0; i < size; i++ {
+		scanner.Scan()
+		line := strings.Fields(scanner.Text())
+		for y := 0; y < size; y++ {
+			matrix[y][i], _ = strconv.Atoi(line[y])
+		}
+	}
+
+	fout, _ := os.Create("output.txt")
+	check := 1
+
+	for i := 0; i < size; i++ {
+		for y := i; y < size; y++ {
+			if matrix[y][i] != matrix[i][y] {
+				fmt.Fprintln(fout, "NO")
+				check = 0
+				break
+			}
+		}
+
+		if matrix[i][i] == 1 {
+			fmt.Fprintln(fout, "NO")
+			check = 0
+			break
+		}
+	}
+
+	if check == 1 {
+		fmt.Fprintln(fout, "YES")
+	}
+}
