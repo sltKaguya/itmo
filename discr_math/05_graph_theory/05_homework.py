f057f993222d1133410a4575d3c3b48d7933cb74
sltKaguya <bottboris@gmail.com>
2022-06-10 16:13:44 +0300

    misc

--- discr_math/05_graph_theory/05_homework.py
+++ discr_math/05_graph_theory/05_homework.py
@@ -1,6 +1,9 @@
+import json
 import networkx as nx
 import matplotlib.pyplot as plt
+from networkx.readwrite import json_graph as nxjson
 
-euroGraph = nx.read_edgelist("05_edge_list.txt")
+euroGraph2 = nx.read_edgelist("05_edge_list2.txt")
+euroGraph2.add_nodes_from(["Cyprus", "Iceland", "Malta"])
 
-#Cyprus, Iceland, Malta
+euroGraph = nx.read_adjlist("05_edge_list.txt", delimiter="--")
