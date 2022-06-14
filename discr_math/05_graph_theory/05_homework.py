1acb797c5823246ce46b53c85d8adec6128c397d
sltKaguya <bottboris@gmail.com>
2022-06-15 01:01:46 +0300

    мне в будущем: удачи понять эти ебучие коммиты сучка

--- discr_math/05_graph_theory/05_homework.py
+++ discr_math/05_graph_theory/05_homework.py
@@ -1,10 +1,7 @@
-import json
 import networkx as nx
 import matplotlib.pyplot as plt
-from networkx.readwrite import json_graph as nxjson
 
-euroGraph2 = nx.read_edgelist("05_edge_list2.txt")
-euroGraph2.add_nodes_from(["Cyprus", "Iceland", "Malta"])
-
-euroGraph = nx.read_adjlist("05_edge_list.txt", delimiter="--")
-nx.draw_spring(euroGraph)
+euroGraph = nx.read_adjlist(
+    "/Users/sltkaguya/Documents/itmo/discr_math/05_graph_theory/eurograph.adjlist.txt", delimiter="--")
+nx.draw_planar(euroGraph, with_labels=True)
+plt.show()
